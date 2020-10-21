2b18cf19ff74c3b1da3c1b39223acc336123adfd
Jaewon <jane3999591@gmail.com>
2020-10-21 15:59:01 +0900

    feat : theme sidebar 배색 리스트 요소 구현 | S03P31A501-36

--- frontend/src/components/sidebar/ThemeSidebar.vue
+++ frontend/src/components/sidebar/ThemeSidebar.vue
@@ -1,6 +1,12 @@
 <template>
-  <div>
-    <h1>TheamSidebar</h1>
+  <div class="theme-list-wrap d-flex justify-center">
+    <div class="theme-wrap d-flex align-center justify-center" :style="{'background-color' : '#55423d'}">
+      <div class="theme color1" :style="{'background-color' : '#281c1b'}"></div>
+      <div class="theme color2" :style="{'background-color' : '#fff3ec'}"></div>
+      <div class="theme color3" :style="{'background-color' : '#9656a1'}"></div>
+      <div class="theme color4" :style="{'background-color' : '#ffc1ad'}"></div>
+      <div class="theme color5" :style="{'background-color' : '#e78fb3'}"></div>
+    </div>
   </div>
 </template>
 
@@ -11,5 +17,49 @@ export default {
 </script>
 
 <style>
+
+.theme-list-wrap{
+  height: 100%;
+  border-right: 3px solid #707070;
+}
+
+.theme-wrap{
+  margin-top: 25px;
+  width: 70%;
+  height: 95px;
+  border: 3px solid #707070;
+}
+
+.theme {
+  width: 65px;
+  height: 65px;
+  border-radius: 75px;
+  border: 2.5px solid #707070;
+  float: left;
+}
+
+.theme.color1{
+  z-index: 50;
+}
+
+.theme.color2{
+  margin-left: -30px;
+  z-index: 51;
+}
+
+.theme.color3{
+  margin-left: -30px;
+  z-index: 52;
+}
+
+.theme.color4{
+  margin-left: -30px;
+  z-index: 53;
+}
+
+.theme.color5{
+  margin-left: -30px;
+  z-index: 54;
+}
 
 </style>
--- frontend/src/views/Landing.vue
+++ frontend/src/views/Landing.vue
@@ -23,8 +23,10 @@ export default {
 
 <style>
 .landing.wrap{
-  width: 100vw;
+  width: 100%;
   height: 100vh;
+  margin: 0;
+  padding: 0;
 }
 
 .theme-sidebar.wrap{
