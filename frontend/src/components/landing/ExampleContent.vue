879d175ae1ee2b6772b90d458e252af3b7ce6159
Jaewon <jane3999591@gmail.com>
2020-10-26 00:15:52 +0900

    feat : flag 이용한 example 컬러 바인딩

--- frontend/src/components/landing/ExampleContent.vue
+++ frontend/src/components/landing/ExampleContent.vue
@@ -1,10 +1,10 @@
 <template>
-  <div class="background" :style="{'background-color': this.demoColor.backColor}">
-    <h1 :style="{'color': this.demoColor.headColor}">colorPOOL</h1>
-    <blockquote :style="{'border-color': this.demoColor.quoteColor, 'background-color': this.quoteBack}">colorPOOL의 빅데이터 풀에서 나의 own 컬러와 배색을 찾고, 나만의 컨텐츠를 만들어보세요</blockquote>
-    <h2 :style="{'color': this.demoColor.subColor}">1. <codeblock :style="{'background-color': this.demoColor.emphaColor}">coloPOOL</codeblock>은 캐주얼한 컨텐츠로 소비될 수있는 서비스를 지향합니다.</h2>
-    <h3>2. <codeblock :style="{'background-color': this.demoColor.emphaColor}">coloPOOL</codeblock>은 멋진 배색을 찾고, 컬러 플레이를 즐기는 사용자를 대상으로 합니다.</h3>
-    <h3>3. <codeblock :style="{'background-color': this.demoColor.emphaColor}">coloPOOL</codeblock>은 온라인에서 다시 오프라인으로 이어지는 유저 경험을 제공합니다.</h3>
+  <div class="background" :style="{'background-color': this.selectedDemoColors[0]}">
+    <h1 :style="{'color': this.selectedDemoColors[1]}">colorPOOL</h1>
+    <blockquote :style="{'border-color': this.demoColors[4], 'background-color': this.quoteBackColor}">colorPOOL의 빅데이터 풀에서 나의 own 컬러와 배색을 찾고, 나만의 컨텐츠를 만들어보세요</blockquote>
+    <h2 :style="{'color': this.selectedDemoColors[2]}">1. <codeblock :style="{'background-color': this.selectedDemoColors[3]}">coloPOOL</codeblock>은 캐주얼한 컨텐츠로 소비될 수있는 서비스를 지향합니다.</h2>
+    <h3>2. <codeblock :style="{'background-color': this.selectedDemoColors[3]}">coloPOOL</codeblock>은 멋진 배색을 찾고, 컬러 플레이를 즐기는 사용자를 대상으로 합니다.</h3>
+    <h3>3. <codeblock :style="{'background-color': this.selectedDemoColors[3]}">coloPOOL</codeblock>은 온라인에서 다시 오프라인으로 이어지는 유저 경험을 제공합니다.</h3>
     <ul>
       <li>메인컬러를 선택하여 배색을 추천받고, 배색을 포함하는 이미지로 매거진을 제작할 수 있습니다.</li>
       <li>편집한 매거진은 인쇄를 통해 실물로 만나 볼 수 있습니다.</li>
@@ -19,49 +19,53 @@ const colorStore = 'colorStore'
 export default {
   name: 'ExampleContent',
   computed:{
-    ...mapGetters(colorStore, {storeDemoColor: 'GE_DEMO_COLOR'}),
+    ...mapGetters(colorStore, {storeDemoColor: 'GE_DEMO_COLOR',
+                                storeDemoFlag: 'GE_DEMO_FLAG',
+                                storeSelectedDemo: 'GE_SELECTED_DEMO'}),
   },
   data(){
     return{
-      demoColor: null,
-      quoteBack: ''
+      demoColors: [], // themeSidebar에서 선택한 theme
+      demoFlag: [], // 해당 color를 적용할지 안할지
+      selectedDemoColors: [], // 선택한 theme에서 적용할 색들만 저장 
+      quoteBackColor: ''
     }
   },
   watch: {
-    storeDemoColor(val){
-      this.demoColor = val;
-      this.appyColor('watch');
+    storeSelectedDemo(val){
+      this.selectedDemoColors = val;
+    }, 
+    storeDemoFlag(val){
+      this.selectedDemoColors = val;
     },
+    storeDemoColor(val){
+      this.selectedDemoColors = val;
+    }
   },
   created(){
-    this.demoColor = this.storeDemoColor;
-    console.log(this.demoColor);
-    this.addOpacity(this.demoColor.quoteColor);
-    this.applyColor('created');
-  },
-  beforeUpdate(){
-    this.applyColor('beforeupdate');
+    this.selectedDemoColors = this.storeSelectedDemo;
   },
   updated() {
-    this.addOpacity(this.demoColor.quoteColor);
-    this.applyColor('updated');
+    this.selectedDemoColors = this.storeSelectedDemo;
   },
   methods:{
     addOpacity(color){
       let temp = color.replace('rgb(', "rgba(");
-      temp = temp.replace(')', ", 0.1)");
-      this.quoteBack = temp;
+      temp = temp.replace(')', ", 0.2)");
+      this.quoteBackColor = temp;
     },
 
     //tag 검색해서 거기에 style 추가하는 함수
     applyColor(temp){
+      temp
       // alert('applyColor : '+temp);
-      console.log('applyColor', temp)
-      if(this.demoColor.backColor!==''){
-        const backGround = document.querySelector('.background');
-        console.log(backGround);
-        backGround.style.backgroundColor=this.demoColor.backColor;
-      }
+      // console.log('applyColor', temp);
+      // if(this.demoColor.backColor!==''){
+      //   const backGround = document.querySelector('.background');
+      //   console.log(backGround);
+      //   backGround.style.backgroundColor=this.demoColor.backColor;
+      // }
+
       // else if(this.demoColor.headColor!==''){
 
       // } else if(this.demoColor.subColor!==''){
