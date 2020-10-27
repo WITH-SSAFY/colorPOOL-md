e6853a86e5b215d83ee7d55366bc9d97827b245d
ynnjn <dbswls1014@naver.com>
2020-10-27 10:47:10 +0900

    feat : Test the function of the tool bar | S03P31A501-42

--- frontend/src/components/edit/Editing.vue
+++ frontend/src/components/edit/Editing.vue
@@ -7,7 +7,20 @@
       <div class="slide-section">
         <div class="tool-box">
           <div class="tool-bar">
-            tool bar
+            <button @click="appendBold"><v-icon>mdi-format-bold</v-icon></button>
+            <button @click="appendItalic"><v-icon>mdi-format-italic</v-icon></button>
+            <button @click="appendUnderline"><v-icon>mdi-format-underline</v-icon></button>
+            <button @click="appendCode"><v-icon>mdi-code-tags</v-icon></button>
+            <button @click="appendH1"><v-icon>mdi-format-header-1</v-icon></button>
+            <button @click="appendH2"><v-icon>mdi-format-header-2</v-icon></button>
+            <button @click="appendH3"><v-icon>mdi-format-header-3</v-icon></button>
+            <button @click="appendH4"><v-icon>mdi-format-header-4</v-icon></button>
+            <button @click="appendH5"><v-icon>mdi-format-header-5</v-icon></button>
+            <button @click="appendH6"><v-icon>mdi-format-header-6</v-icon></button>
+            <button @click="appendQuote"><v-icon>mdi-format-quote-open</v-icon></button>
+            <button @click="appendUl"><v-icon>mdi-format-list-bulleted</v-icon></button>
+            <button @click="appendOl"><v-icon>mdi-format-list-numbered</v-icon></button>
+            <button @click="appendEmoji"><v-icon>mdi-emoticon-outline</v-icon></button>
           </div>
           <div class="import-button">
             import md
@@ -46,15 +59,66 @@ export default {
     }
   },
   methods: {
-    updateInput: function(event) {
+    updateInput () {
       var updatedText = event.target.value;
-      this.inputText = updatedText
+      this.inputText = updatedText;
       document.querySelector("#page").innerHTML = md.render(this.inputText.toString());
+    },
+    appendBold () {
+      // this.inputText = '**'.concat(this.inputText, '**')
+      // document.querySelector("#page").innerHTML = md.render(this.inputText.toString());
+      this.inputText += '\n** bold example';
+      this.inputText += '\n**';
+    },
+    appendItalic () {
+      this.inputText += '\n_ bold example';
+      this.inputText += '\n_';
+    },
+    appendUnderline () {
+      this.inputText += '\n~~ bold example';
+      this.inputText += '\n~~';
+    },
+    appendCode () {
+      this.inputText += '\n ``` js\n';
+      this.inputText += "var foo = function (bar) {\nreturn bar++;\n};\nconsole.log(foo(5));";
+      this.inputText += '\n ```';
+    },
+    appendH1 () {
+      this.inputText += '\n# h1 example'
+    },
+    appendH2 () {
+      this.inputText += '\n## h2 example'
+    },
+    appendH3 () {
+      this.inputText += '\n### h3 example'
+    },
+    appendH4 () {
+      this.inputText += '\n#### h4 example'
+    },
+    appendH5 () {
+      this.inputText += '\n##### h5 example'
+    },
+    appendH6 () {
+      this.inputText += '\n###### h6 example'
+    },
+    appendQuote () {
+      this.inputText += '\n > quote example'
+    },
+    appendUl () {
+      this.inputText += '\n- bullet example'
+    },
+    appendOl () {
+      this.inputText += '\n1. number example'
+    },
+    appendEmoji() {
+      let emojis = [':wink:', ':cry:', ':laughing:', ':yum:'];
+      this.inputText += '\n' + emojis[Math.floor(Math.random() * emojis.length)];
     }
   },
-  created() {
-    // test
-    // var str = "# h1 Heading \n## h2 Heading Heading \n### h3 Heading \n#### h4 Heading \n##### h5 Heading \n###### h6 Heading \n---"
+  watch: {
+    inputText() {
+      document.querySelector("#page").innerHTML = md.render(this.inputText.toString());
+    }
   }
 }
 </script>
@@ -90,6 +154,10 @@ export default {
   background-color: lightgrey;
   height: 4vh;
   width: 50%;
+}
+
+.editing-area.col .tool-box .tool-bar button {
+  padding: 1% 1%;
 }
 
 .editing-area.col .tool-box .import-button {
@@ -135,7 +203,10 @@ padding-top = 세로 / 가로 * 100
   bottom: 0;
 }
 
-
-
+* >>> blockquote {
+  border-left: 8px solid #ccc;
+  margin: 10px;
+  padding: 10px;
+}
 
 </style>
