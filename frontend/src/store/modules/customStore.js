20e7b2f54e4e0523a3eb95304f695c9f283263e0
seeung0305 <seeung0305@naver.com>
2020-11-02 16:06:08 +0900

    feat : select color router

--- frontend/src/store/modules/customStore.js
+++ frontend/src/store/modules/customStore.js
@@ -2,18 +2,27 @@ const customStore = {
   namespaced: true,
   state: {
     customTheme: null,
+    finalTheme: null,
   },
   getters: {
     GE_CUSTOM_THEME: state => state.customTheme,
+    GE_FINAL_THEME: state => state.finalTheme,
   },
   mutations: {
     MU_CUSTOM_THEME: (state, payload) => {
       state.customTheme = payload;
-    }
+    },
+    MU_FINAL_THEME: (state, payload) => {
+      state.finalTheme = payload;
+      console.log('MU_FINAL_THEME', state.finalTheme)
+    },
   },
   actions: {
     AC_CUSTOM_THEME: ({commit}, payload) => {
       commit('MU_CUSTOM_THEME', payload);
+    },
+    AC_FINAL_THEME: ({commit}, payload) => {
+      commit('MU_FINAL_THEME', payload);
     }
   }
 }
