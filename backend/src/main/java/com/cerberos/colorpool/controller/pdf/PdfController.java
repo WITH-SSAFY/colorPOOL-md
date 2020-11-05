d1e3555c18c63587144aa29b74a75976f0c987ec
배현석 <qogustjr119@naver.com>
2020-11-06 01:18:42 +0900

    feat : image upload test and apply css to pdf are done

--- backend/src/main/java/com/cerberos/colorpool/controller/pdf/PdfController.java
+++ backend/src/main/java/com/cerberos/colorpool/controller/pdf/PdfController.java
@@ -7,18 +7,25 @@ import com.cerberos.colorpool.service.pdf.PdfService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 @RestController
 @CrossOrigin
 @RequiredArgsConstructor
-@RequestMapping(value = "/api/pdf")
+@RequestMapping(value = "/api")
 public class PdfController {
     private final PdfService pdfService;
     private final ResponseService responseService;
 
     @Operation(summary = "마크다운 및 pdf저장", description = "사용자가 작성한 마크다운의 내용을 저장하고 이를 pdf로 변환하여 저장한 후 pdf저장 경로를 반환합니다.")
-    @PostMapping("/save")
-    public SingleResult<String> savePDF(@RequestBody PdfModel pdfModel){
-        return responseService.getSingleResult(pdfService.savePDF(pdfModel));
+    @PostMapping("/pdf")
+    public SingleResult<String> uploacPdf(@RequestBody PdfModel pdfModel){
+        return responseService.getSingleResult(pdfService.uploadPdf(pdfModel));
+    }
+
+    @Operation(summary = "로컬 이미지 업로드", description = "사용자가 업로드한 로컬 이미지를 저장하고 이미지 경로를 반환합니다.")
+    @PostMapping("/image")
+    public SingleResult<String> uploadImage(){ //@RequestParam MultipartFile imageMultipartFile
+        return responseService.getSingleResult(pdfService.uploadImage());
     }
 }
