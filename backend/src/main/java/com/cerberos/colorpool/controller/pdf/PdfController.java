628b312d5eca3169bdabe1a9d1ba42800e80bb15
배현석 <qogustjr119@naver.com>
2020-11-09 17:48:52 +0900

    fix : remove uploadImage API

--- backend/src/main/java/com/cerberos/colorpool/controller/pdf/PdfController.java
+++ backend/src/main/java/com/cerberos/colorpool/controller/pdf/PdfController.java
@@ -3,6 +3,7 @@ package com.cerberos.colorpool.controller.pdf;
 import com.cerberos.colorpool.model.pdf.PdfModel;
 import com.cerberos.colorpool.model.response.SingleResult;
 import com.cerberos.colorpool.service.ResponseService;
+import com.cerberos.colorpool.service.image.ImageService;
 import com.cerberos.colorpool.service.pdf.PdfService;
 import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
@@ -21,11 +22,5 @@ public class PdfController {
     @PostMapping("/pdf")
     public SingleResult<String> uploacPdf(@RequestBody PdfModel pdfModel){
         return responseService.getSingleResult(pdfService.uploadPdf(pdfModel));
-    }
-
-    @Operation(summary = "로컬 이미지 업로드", description = "사용자가 업로드한 로컬 이미지를 저장하고 이미지 경로를 반환합니다.")
-    @PostMapping("/image")
-    public SingleResult<String> uploadImage(){ //@RequestParam MultipartFile imageMultipartFile
-        return responseService.getSingleResult(pdfService.uploadImage());
     }
 }
