5ac36bb438bc4cd39d7d2e6de6dd9466886fd238
배현석 <qogustjr119@naver.com>
2020-11-10 15:26:28 +0900

    feat : batch system implemented Contents - delete All pdf files at 3:00p.m.(in Seoul 0:00a.m.)

---
+++ backend/src/main/java/com/cerberos/colorpool/service/schedule/ScheduleService.java
@@ -0,0 +1,37 @@
+package com.cerberos.colorpool.service.schedule;
+
+import com.cerberos.colorpool.entity.pdf.Pdf;
+import com.cerberos.colorpool.repository.pdf.PdfJpaRepository;
+import com.cerberos.colorpool.s3.S3api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class ScheduleService {
+    private final PdfJpaRepository pdfJpaRepository;
+    private final S3api s3api;
+
+    @Scheduled(cron = "0 0 15 * * *")
+    public void clearPdfBucket(){
+        List<Pdf> allPdfList = pdfJpaRepository.findAll();
+        String keyPrefix = "pdf/";
+        String subPath = "/pdf";
+
+        List<String> allPdfNameList = new ArrayList<>();
+        if(!allPdfList.isEmpty()){
+            for(Pdf pdf : allPdfList){
+                String[] splitedPath = pdf.getPath().split("[/]");
+                String pdfName = splitedPath[splitedPath.length-1];
+                allPdfNameList.add(keyPrefix+pdfName);
+            }
+            s3api.delete(allPdfNameList,subPath);
+            pdfJpaRepository.deleteAll();
+        }
+    }
+}
